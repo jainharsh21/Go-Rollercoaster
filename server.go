91018b0eb99558a1e7ebe9ea9472ab95a938317b
jainharsh21 <jainharsh270@gmail.com>
2020-09-15 13:19:57 +0530

    get all coasters

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 import (
 	"net/http"
+	"encoding/json"
 )
 
 // Coaster type
@@ -12,14 +13,46 @@ type Coaster struct {
 	Height       int    `json:"height"`
 }
 
+type coasterHandlers struct{
+	store map[string]Coaster
+}
 
+func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request){
 
-func coastersHandler(w http.ResponseWriter, r *http.Request){
+	coasters := make([]Coaster,len(h.store))
+	i := 0
+	for _,coaster := range h.store{
+		coasters[i] = coaster
+		i++
+	}
 
+	jsonBytes,err := json.Marshal(coasters)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+	w.Header().Add("content-type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(jsonBytes)
+}
+
+func newCoasterHandlers() *coasterHandlers{
+	return &coasterHandlers{
+		store : map[string]Coaster{
+			"id1" : Coaster{
+				Name: "Coaster Name",
+				Height: 99,
+				ID : "id1",
+				InPark : "Park Name",
+				Manufacturer: "Manufacturer Name",
+			},
+		},
+	}
 }
 
 func main()  {
-	http.HandleFunc("/coasters",coastersHandler)
+	coasterHandlers := newCoasterHandlers()
+	http.HandleFunc("/coasters",coasterHandlers.get)
 	err := http.ListenAndServe(":8080",nil)
 	if err != nil {
 		panic(err)
